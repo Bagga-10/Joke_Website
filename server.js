96a370a75e65fd66dab8b40e69021d5097414a8f
“[Syed <syed88704@gmail.com>
2025-04-17 16:41:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import axios from 'axios';
+
+const app=express();
+const PORT=3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/api/joke', async (req,res) =>{
+    const {name}=req.body;
+    try{
+        const response = await axios.get('https://v2.jokeapi.dev/joke/Any?format=txt')
+        let joke =response.data;
+
+        let PersonalizedJoke = joke.replace(/Chuck Norris>/g, name || 'Someone');
+
+        res.send({joke:PersonalizedJoke});
+    }catch(error){
+        res.status(500).send({error:'Error fetching joke'});
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
